f5bc2e6956c18278114852db357e39144067c5b7
Phillip Rogers <phillip.l.rogers.29@gmail.com>
2025-08-05 17:12:54 -0700

    Trying to redirects working. Update to DATABASES to test locally as well.

--- pickleball-backend/config/settings.py
+++ pickleball-backend/config/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
-
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",  # 👈 fallback for local dev
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
